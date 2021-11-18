986cdc4a17a389e6ef341653c7286735e93b1b29
agrawal-atharva <atharva.agrawal@tothenew.com>
2021-11-18 13:40:17 +0530

    rest api q1

---
+++ rest-api/Question1/backend/index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const port = process.env.PORT || 5000;
+const users = require('./userData');
+const data = JSON.stringify(users);
+
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(data));
+})
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}!!!`);
+    console.log(data);
+});
---
+++ rest-api/Question1/backend/userData.js
@@ -0,0 +1,28 @@
+var users = [
+	{
+		username: 'atharva_agrawal',
+		password: 'atharva123',
+		firstName: 'Atharva',
+		lastName: 'Agrawal',
+	},
+    {
+		username: 'bharat_agrawal',
+		password: 'bharat123',
+		firstName: 'Bharat',
+		lastName: 'Agrawal',
+	},
+    {
+		username: 'aayush_agrawal',
+		password: 'aayush123',
+		firstName: 'Aayush',
+		lastName: 'Agrawal',
+	},
+    {
+		username: 'piyush_agrawal',
+		password: 'piyush123',
+		firstName: 'Piyush',
+		lastName: 'Agrawal',
+	},
+];
+
+module.export = users;
